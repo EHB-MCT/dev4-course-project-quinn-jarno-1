5d0d01c0bcc7219242606e3d49bb344fd817921e
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-04-25 15:37:23 +0200

    Created IntelliJ Project

---
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/Dev4CourseProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.ehb.dev4courseproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dev4CourseProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<Dev4CourseProjectApplication>(*args)
+}
---
+++ Backend/Dev4CourseProject/src/test/kotlin/com/ehb/dev4courseproject/Dev4CourseProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ehb.dev4courseproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Dev4CourseProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
