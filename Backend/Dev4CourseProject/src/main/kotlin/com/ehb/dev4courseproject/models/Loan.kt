824fb3afeee1183e4ec6e658a5bc2027b14d3a3d
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-06 17:17:34 +0200

    Created Loan Model
    Created Loan model and added variables, the date when the loan is created and for when the item needs to be returned and a Boolean for whether the item has been returned or not.

---
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/models/Loan.kt
@@ -0,0 +1,16 @@
+package com.ehb.dev4courseproject.models
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "loans")
+data class Loan(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = -1,
+
+    val loanDate: LocalDateTime = LocalDateTime.now(),
+    val dueDate: LocalDateTime,
+    var returned: Boolean = false,
+){
+}
