ab60e42c5b544283aa497e4f8e49a4961788119a
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-07 18:14:09 +0200

    Removed Location Var
    Removed the location variable

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/dto/CreateItemRequest.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/dto/CreateItemRequest.kt
@@ -2,7 +2,6 @@ package com.ehb.dev4courseproject.dto
 
 data class CreateItemRequest(
     var name: String,
-    var location: String,
     var description: String,
     var isLoanedOut: Boolean
 )
