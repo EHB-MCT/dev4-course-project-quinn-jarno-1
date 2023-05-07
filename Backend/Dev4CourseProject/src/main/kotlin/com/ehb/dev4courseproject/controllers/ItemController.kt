8b02f41efddbef9864c463a6030af31106cd2bca
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-07 18:43:05 +0200

    ItemController Added Get and Post
    Added Get and Post mapping to get all items and add an item to the database.

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/ItemController.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/ItemController.kt
@@ -1,4 +1,28 @@
 package com.ehb.dev4courseproject.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+import com.ehb.dev4courseproject.services.ItemService
+import com.ehb.dev4courseproject.models.Item
+import com.ehb.dev4courseproject.dto.CreateItemRequest
+
+@RestController
+@RequestMapping("items")
 class ItemController {
+
+    @Autowired
+    lateinit var itemService: ItemService;
+
+    @GetMapping
+    fun index(): List<Item> {
+        val items = itemService.getAllItems();
+        return items;
+    }
+
+    @PostMapping
+    fun addItem(@RequestBody itemRequest: CreateItemRequest): Item {
+        return itemService.addItem(itemRequest)
+    }
+
 }
