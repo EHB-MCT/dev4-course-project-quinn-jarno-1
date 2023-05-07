250a3daff0a1aa1dab19c5fb37c0bcfc0ff187f5
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-07 18:16:37 +0200

    ItemService Added Get and Add functions
    Added a function to get all the Items in the database and a function to add an item to the db

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/services/ItemService.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/services/ItemService.kt
@@ -1,4 +1,26 @@
 package com.ehb.dev4courseproject.services
 
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+import com.ehb.dev4courseproject.repositories.ItemRepository
+import com.ehb.dev4courseproject.models.Item
+import com.ehb.dev4courseproject.dto.CreateItemRequest
+
+@Service
 class ItemService {
+
+    @Autowired
+    lateinit var itemRepository: ItemRepository
+
+    fun getAllItems(): List<Item> {
+        return itemRepository.findAll()
+    }
+
+    fun addItem(item: CreateItemRequest): Item {
+        val item = Item(name = item.name, description = item.description, isLoanedOut = item.isLoanedOut);
+        return itemRepository.save(item)
+    }
+
 }
