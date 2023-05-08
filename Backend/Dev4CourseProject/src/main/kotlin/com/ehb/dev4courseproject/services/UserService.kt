1ef2c0c8fac8ad900cbfb08d2e57bb047e917ce0
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-08 18:00:45 +0200

    UserService Added Get All And Create Functions
    Added a function to get all users and to create a new user.

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/services/UserService.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/services/UserService.kt
@@ -1,4 +1,25 @@
 package com.ehb.dev4courseproject.services
 
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+import com.ehb.dev4courseproject.models.User
+import com.ehb.dev4courseproject.repositories.UserRepository
+import com.ehb.dev4courseproject.dto.CreateUserRequest
+
+@Service
 class UserService {
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    fun getAllUsers(): List<User> {
+        return userRepository.findAll()
+    }
+
+    fun createUser(user: CreateUserRequest): User {
+        val user = User(username = user.username, email = user.email)
+        return userRepository.save(user)
+    }
 }
