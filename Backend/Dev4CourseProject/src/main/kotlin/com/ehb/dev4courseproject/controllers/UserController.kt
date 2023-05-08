b0118e05f020c853e8550243888ad1723fe47d5b
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-08 18:02:32 +0200

    UserController Added Get And Post
    Added A Get and Post Mapping to get all users and save a new user.

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/UserController.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/UserController.kt
@@ -1,4 +1,29 @@
 package com.ehb.dev4courseproject.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+import com.ehb.dev4courseproject.models.User
+import com.ehb.dev4courseproject.services.UserService
+import com.ehb.dev4courseproject.dto.CreateUserRequest
+
+@RestController
+@RequestMapping("users")
 class UserController {
+
+    @Autowired
+    lateinit var userService: UserService;
+
+    @GetMapping
+    fun getUsers(): List<User> {
+        //Get all users
+        val users = userService.getAllUsers();
+        return users;
+    }
+
+    @PostMapping
+    fun saveUser(@RequestBody userRequest: CreateUserRequest): User {
+        return userService.createUser(userRequest)
+    }
 }
