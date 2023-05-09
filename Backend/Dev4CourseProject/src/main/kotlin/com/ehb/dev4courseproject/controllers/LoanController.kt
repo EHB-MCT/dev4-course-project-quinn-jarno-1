fd9bf3c7305eab8438db0502e755bbd9fcf542ce
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-09 14:17:50 +0200

    LoanController added mapping
    Added a Get and Post mapping to get all loans, create a new loan and return a loan.

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
@@ -1,4 +1,34 @@
 package com.ehb.dev4courseproject.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+import com.ehb.dev4courseproject.dto.NewLoanRequest
+import com.ehb.dev4courseproject.models.Loan
+import com.ehb.dev4courseproject.services.LoanService
+
+@RestController
+@RequestMapping("loans")
 class LoanController {
+
+    @Autowired
+    lateinit var loanService: LoanService;
+
+    @GetMapping
+    fun index(): List<Loan> {
+        //Get all loans
+        val loans = loanService.getLoans();
+
+        return loans;
+    }
+
+    @PostMapping
+    fun loanItem(@RequestBody loanRequest: NewLoanRequest): Loan {
+        return loanService.newLoan(loanRequest)
+    }
+
+    @PostMapping("/return/{loanId}")
+    fun returnLoan(@PathVariable loanId: Long) {
+        loanService.returnLoan(loanId)
+    }
 }
