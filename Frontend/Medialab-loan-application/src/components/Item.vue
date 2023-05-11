d34acbe2972edc65306500378de9f1f91316eee4
QuinnJan <quinn.janssens@student.ehb.be>
2023-05-11 12:07:11 +0100

    Item Component

---
+++ Frontend/Medialab-loan-application/src/components/Item.vue
@@ -0,0 +1,31 @@
+<!-- EEN ITEM COMPONENT -->
+
+<script>
+    export default {
+        props: {
+            name: String,
+            description: String,
+            isLoanedOut: Boolean,
+        }
+    }
+</script>
+
+<template>
+    <div class="item">
+        <h4>{{ name }}</h4>
+        <p>{{ description }}</p>
+        <p>{{ isLoanedOut }}</p>
+    </div>
+</template>
+
+<style scoped>
+    .item {
+        display: flex;
+        align-items: center;
+    }
+
+    .item > * {
+        padding: 12px;
+    }
+
+</style>
