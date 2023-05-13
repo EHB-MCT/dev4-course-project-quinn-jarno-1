5047e9ccbbbbb0290bbd848eadd289c4234646bb
JarnoPauwels <jarno.pauwels@student.ehb.be>
2023-05-13 12:54:55 +0200

    Added Comments
    Added Comments Explaining what the functions do.

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
@@ -14,19 +14,20 @@ class LoanController {
     @Autowired
     lateinit var loanService: LoanService;
 
+    // Get All Loans
     @GetMapping
-    fun index(): List<Loan> {
-        //Get all loans
+    fun getAllLoans(): List<Loan> {
         val loans = loanService.getLoans();
-
         return loans;
     }
 
+    // Create A New Loan
     @PostMapping
     fun loanItem(@RequestBody loanRequest: NewLoanRequest): Loan {
         return loanService.newLoan(loanRequest)
     }
 
+    // Return Loan (Sets Item isLoanedOut to false and Loan Returned to True)
     @PostMapping("/return/{loanId}")
     fun returnLoan(@PathVariable loanId: Long) {
         loanService.returnLoan(loanId)
