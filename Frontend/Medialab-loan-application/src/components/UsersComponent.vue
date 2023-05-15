e75b229942465a7ea694342040ae9362c1b758da
QuinnJan <quinn.janssens@student.ehb.be>
2023-05-15 18:38:56 +0100

    Update User Component

--- Frontend/Medialab-loan-application/src/components/UsersComponent.vue
+++ Frontend/Medialab-loan-application/src/components/UsersComponent.vue
@@ -1,60 +1,45 @@
-<!-- eslint-disable no-irregular-whitespace -->
-
 <script>
 export default {
 data() {
 
 return {
-
  users: []
-
  }
-
 },
 
-// eslint-disable-next-line no-irregular-whitespace
-
-
 mounted() {
-// Make API request to fetch user data
- fetch('http://localhost:9000/users')
- .then(response => response.json())
-.then(data => {
- // Store response data in component's data object
-this.users = data;
- console.log(data);
-})
+    this.getUsers();
+},
 
- .catch(error => {
-
- // Handle any errors that occur during the API request
- console.error(error);
-
- });
-
- },
-
-
- updated() {
-
-// Check that users data is being properly stored in component's data object
-
- console.log(this.users);
-
-}
+ methods: {
+  getUsers(){
+    fetch('http://localhost:9000/users')
+    .then(response => response.json())
+    .then(data => {
+      this.users = data;
+      console.log(data);
+    })
+    .catch(error => {
+    console.error(error);
+    });
+  }
+},
+updated() {
+  console.log(this.users);
  }
+  }
 
 </script>
 
 <template>
     <div>
       <ul>
-            <li v-for="user in users" v-bind:key="user">
+         <li v-for="user in users" v-bind:key="user">
               <h3>{{ user.username }}</h3>
               <p>{{ user.email }}</p>
               <p>{{ user.role }}</p>
               <hr>
-            </li>
+         </li>
           </ul>
     </div>
 </template>
