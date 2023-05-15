c672442712a26886605db3a17055321456182156
QuinnJan <quinn.janssens@student.ehb.be>
2023-05-15 13:03:02 +0100

    Changed App.vue

--- Frontend/Medialab-loan-application/src/App.vue
+++ Frontend/Medialab-loan-application/src/App.vue
@@ -1,7 +1,49 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import HelloWorld from './components/BannerComponent.vue'
+import app from './components/ItemsComponent.vue'
+import UsersComponent from './components/UsersComponent.vue'
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/ehblogo.jpg" width="225" height="225" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Medialab Loan Application" />
+    </div>
+  </header>
+
+  <main>  
+    <app/>
+    <UsersComponent/>
+  </main>
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
