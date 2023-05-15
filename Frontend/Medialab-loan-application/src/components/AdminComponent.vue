5bc451f8c83e05c125735f0470b15f38278eac19
JarnoPauwels <jarno.pauwels@student.ehb.be>
2023-05-15 22:22:47 +0200

    Admin Page
    Added admin view and component with methods to get items, users and loans.

---
+++ Frontend/Medialab-loan-application/src/components/AdminComponent.vue
@@ -0,0 +1,383 @@
+<script>
+
+export default {
+
+  data() {
+    return {
+
+      items: [],
+      users: [],
+      selectedItemId: null,
+      updatedItem: {
+        id: null,
+        name: "",
+        description: "",
+        isLoanedOut: false
+      }
+
+    }
+  },
+
+  mounted() {
+    this.getItems();
+    this.getUsers();
+    this.getLoans();
+  },
+  methods: {
+    // Gets All Items
+    getItems() {
+      fetch('http://localhost:9000/items')
+        .then(response => response.json())
+        .then(data => {
+          this.items = data;
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+
+    getUsers() {
+      fetch('http://localhost:9000/users')
+        .then(response => response.json())
+        .then(data => {
+          this.users = data;
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+
+    getLoans() {
+      fetch('http://localhost:9000/loans')
+        .then(response => response.json())
+        .then(data => {
+          this.loans = data;
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+
+    returnItem(loanId) {
+      console.log(loanId);
+    },
+
+    // Get Logged In User
+    // getUser() {
+    //   fetch(`http://localhost:9000/users/token/${localStorage.getItem('authToken')}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       this.user = data;
+    //       console.log(data);
+    //     })
+    //     .catch(error => {
+    //       console.error(error);
+    //     });
+    // },
+
+    // Lend out an item
+    // loanItem(userId, itemId) {
+    //   const token = localStorage.getItem('authToken');
+    //   fetch('http://localhost:9000/loans', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': `${token}`
+    //     },
+    //     body: JSON.stringify({ userId: userId, itemId: itemId }),
+    //   })
+    //     .then(response => {
+    //       if (response.ok) {
+    //         console.log("SUCCES" + response)
+    //         return response.text();
+    //       } else {
+    //         throw new Error(`Invalid credentials ${userId} ${itemId} ${token}`);
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error(error);
+    //     });
+    // },
+
+    // Delete Item
+    deleteItem(itemId) {
+      fetch(`http://localhost:9000/items/${itemId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => {
+          if (response.ok) {
+            const index = this.items.findIndex(item => item.id === itemId);
+            if (index !== -1) {
+              this.items.splice(index, 1);
+            }
+          } else {
+            console.error('Item deletion failed: Item may be Loaned Out');
+          }
+        }).catch(error => {
+          console.error(error);
+        });
+    },
+
+    // Edit Item
+    editItem(item) {
+      this.selectedItemId = item.id;
+      this.updatedItem = { ...item };
+      console.log("response OK")
+    },
+
+    updateItem() {
+      fetch(`http://localhost:9000/items/update/${this.selectedItemId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.updatedItem)
+      })
+        .then(response => {
+          if (response.ok) {
+            console.log("response OK")
+            const index = this.items.findIndex(item => item.id === this.updatedItem.id);
+            if (index !== -1) {
+              this.items.splice(index, 1, this.updatedItem);
+            }
+            this.cancelEdit();
+          } else {
+            console.error('Item update failed.');
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    cancelEdit() {
+      this.selectedItemId = null;
+      this.updatedItem = {
+        id: null,
+        name: "",
+        description: "",
+        loanStatus: false
+      };
+
+    },
+    // this doesnt get updated through the updateItem method but through the create loan method
+    setLoanStatus(isLoanedOut) {
+      this.updatedItem.isLoanedOut = isLoanedOut;
+    }
+  },
+
+  updated() {
+
+    // console.log(this.items);
+  }
+}
+
+
+</script>
+
+<template>
+  <h2>Items overview</h2>
+  <div class="item">
+    <ul>
+      <li v-for="item in items" v-bind:key="item">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+        <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+        <span class="available" v-else>Available</span>
+        <button class="update-button" @click="editItem(item)">Update</button>
+        <div v-if="item.id === selectedItemId" class="edit-form">
+          <input v-model="updatedItem.name" type="text" placeholder="Name" />
+          <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
+          <div class="form-buttons">
+            <button class="save-button" @click="updateItem">Save</button>
+            <button class="cancel-button" @click="cancelEdit">Cancel</button>
+          </div>
+        </div>
+        <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
+        <!-- <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button> -->
+        <hr>
+      </li>
+    </ul>
+  </div>
+
+  <h2>Users overview</h2>
+  <div class="item">
+    <ul>
+      <li v-for="user in users" v-bind:key="user">
+        <h3>{{ user.username }}</h3>
+        <p>{{ user.email }}</p>
+        <p>{{ user.role }}</p>
+        <hr>
+        <!-- <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+        <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+        <span class="available" v-else>Available</span>
+        <button class="update-button" @click="editItem(item)">Update</button>
+        <div v-if="item.id === selectedItemId" class="edit-form">
+          <input v-model="updatedItem.name" type="text" placeholder="Name" />
+          <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
+          <div class="form-buttons">
+            <button class="save-button" @click="updateItem">Save</button>
+            <button class="cancel-button" @click="cancelEdit">Cancel</button>
+          </div>
+        </div>
+        <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
+        <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button>
+        <hr> -->
+      </li>
+    </ul>
+  </div>
+
+  <h2>Loans overview</h2>
+  <div class="item">
+    <ul>
+      <li v-for="loan in loans" v-bind:key="loan">
+        <h3>Loan: {{ loan.id }}</h3>
+        <h4>Item with item name: {{ loan.item.name }} and ID: {{ loan.item.id }} Loaned out By User with username: {{ loan.user.username }} and ID: {{ loan.user.id }}</h4>
+        <button class="" @click="returnItem(loan.id)">Item Returned</button>
+        <hr>
+        <!-- <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+      <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+      <span class="available" v-else>Available</span>
+      <button class="update-button" @click="editItem(item)">Update</button>
+      <div v-if="item.id === selectedItemId" class="edit-form">
+        <input v-model="updatedItem.name" type="text" placeholder="Name" />
+        <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
+        <div class="form-buttons">
+          <button class="save-button" @click="updateItem">Save</button>
+          <button class="cancel-button" @click="cancelEdit">Cancel</button>
+        </div>
+      </div>
+      <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
+      <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button>
+      <hr> -->
+      </li>
+    </ul>
+  </div>
+
+
+
+</template>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 20px;
+}
+
+h2 {
+  font-size: 40px;
+  margin-bottom: 20px;
+  font-weight: bold;
+}
+
+h3 {
+  font-size: 20px;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+p {
+  margin-bottom: 10px;
+}
+
+span {
+  font-weight: lighter;
+  margin-bottom: 5px;
+}
+
+.delete-button {
+  background-color: hsl(0, 0%, 0%);
+  color: rgb(255, 255, 255);
+  border: none;
+  padding: 8px 16px;
+  margin-left: 3px;
+  margin-bottom: -10px;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+.delete-button:hover {
+  background-color: #a90000;
+}
+
+.update-button {
+  background-color: hsl(0, 0%, 0%);
+  color: rgb(255, 255, 255);
+  border: none;
+  padding: 8px 16px;
+  margin-left: 3px;
+  margin-bottom: -10px;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+.update-button:hover {
+  background-color: #a90000;
+}
+
+.edit-form {
+  background-color: #f8f8f8;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+}
+
+.edit-form input,
+.edit-form textarea {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.edit-form .form-buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.edit-form .form-buttons button {
+  margin-left: 10px;
+}
+
+.save-button {
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.cancel-button {
+  background-color: #f44336;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.unavailable {
+  color: red;
+  font-weight: bold;
+}
+
+.available {
+  color: green;
+  font-weight: bold;
+}
+</style>
---
+++ Frontend/Medialab-loan-application/src/views/AdminView.vue
@@ -0,0 +1,47 @@
+<script setup>
+    import HelloWorld from '../components/BannerComponent.vue'
+    import AdminComponent from '../components/AdminComponent.vue'
+</script>
+
+<template>
+    <header>
+        <img alt="Vue logo" class="logo" src="../assets/ehblogo.jpg" width="225" height="225" />
+
+        <div class="wrapper">
+            <HelloWorld msg="Admin Dashboard" />
+        </div>
+    </header>
+
+    <main>
+        <AdminComponent />
+    </main>
+</template>
+
+<style scoped>
+header {
+    line-height: 1.5;
+}
+
+.logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
