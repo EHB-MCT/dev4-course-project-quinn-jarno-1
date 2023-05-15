01f5ba947da281c1d20c3952b1d15f56992827af
QuinnJan <quinn.janssens@student.ehb.be>
2023-05-15 16:23:39 +0100

    Authentication

--- Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
+++ Backend/Dev4CourseProject/src/main/kotlin/com/ehb/dev4courseproject/controllers/LoanController.kt
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.*
 import com.ehb.dev4courseproject.dto.NewLoanRequest
 import com.ehb.dev4courseproject.models.Loan
 import com.ehb.dev4courseproject.services.LoanService
+import com.ehb.dev4courseproject.services.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
+import java.net.http.HttpHeaders
 
+@CrossOrigin(origins = ["http://localhost:5173"])
 @RestController
 @RequestMapping("loans")
 class LoanController {
@@ -23,8 +28,12 @@ class LoanController {
 
     // Create A New Loan
     @PostMapping
-    fun loanItem(@RequestBody loanRequest: NewLoanRequest): Loan {
-        return loanService.newLoan(loanRequest)
+    fun loanItem(@RequestBody loanRequest: NewLoanRequest, @RequestHeader(HttpHeaders.AUTHORIZATION) token: String): Loan {
+        if(UserService.isAuthenticated(token)){
+            return loanService.newLoan(loanRequest)
+        } else {
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authentication token not present / allowed")
+        }
     }
 
     // Return Loan (Sets Item isLoanedOut to false and Loan Returned to True)
