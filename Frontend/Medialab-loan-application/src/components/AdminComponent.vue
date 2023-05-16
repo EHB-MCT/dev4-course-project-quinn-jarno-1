90639a8da3010260c1505d53f818b3c4f0c08c54
JarnoPauwels <jarno.pauwels@student.ehb.be>
2023-05-16 20:23:10 +0200

    Fixed Styling

--- Frontend/Medialab-loan-application/src/components/AdminComponent.vue
+++ Frontend/Medialab-loan-application/src/components/AdminComponent.vue
@@ -109,16 +109,17 @@ export default {
         body: JSON.stringify({ loanId: loanId}),
       })
         .then(response => {
+          console.log(response);
           if (response.ok) {
-            const index = this.loanedItems.findIndex(loan => loan.id === loanId);
-            if (index !== -1) {
-              this.loanedItems.splice(index, 1);
-            }
-            console.log("SUCCES" + response)
+            // const index = this.loanedItems.findIndex(loan => loan.id === loanId);
+            // if (index !== -1) {
+            //   this.loanedItems.splice(index, 1);
+            // }
+            // console.log("SUCCES" + response)
             return response.text();
            
           } else {
-            throw new Error(`Invalid credentials ${loanId}`);
+            throw new Error(response);
           }
         })
         .catch(error => {
@@ -288,11 +289,21 @@ export default {
       <div v-show="showAllItems">
       <ul>
         <li v-for="item in items" v-bind:key="item">
-          <h3>{{ item.name }}</h3>
-          <p>{{ item.description }}</p>
-          <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
-          <span class="available" v-else>Available</span>
-          <button class="update-button" @click="editItem(item)">Update</button>
+          <div class="item-list">
+            <div class="item-list-info">
+              <h3>{{ item.name }}</h3>
+              <p>{{ item.description }}</p>
+              <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+              <span class="available" v-else>Available</span>
+            </div>
+  
+            <div class="item-list-buttons">            
+              <button class="modal-button" @click="showModal = true">Delete</button>
+              <button class="update-button" @click="editItem(item)">Update</button>
+            </div>
+          </div>
+          
+          <!-- Edit Form -->
           <div v-if="item.id === selectedItemId" class="edit-form">
             <input v-model="updatedItem.name" type="text" placeholder="Name" />
             <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
@@ -302,17 +313,17 @@ export default {
             </div>
           </div>
 
-          <button class="modal-button" @click="showModal = true">Delete</button>
+          <!-- Delete -->
           <div v-if="showModal" class="modal">
             <div class="modal-content">
               <!-- Modal content goes here -->
               <h1>Are you sure you want to delete this item?</h1>
-              <button class="modal-button" @click="showModal = false">Cancel</button>
+              <button class="modal-cancel-button" @click="showModal = false">Cancel</button>
               <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
             </div>
           </div>
           <!-- <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button> -->
-          <hr>
+          <!-- <hr> -->
         </li>
       </ul>
     </div>
@@ -324,10 +335,20 @@ export default {
       <div v-show="showAllUsers">
       <ul>
         <li v-for="uniqueUser in users" :key="user">
-          <h3>{{ uniqueUser.username }}</h3>
-          <p>Email: {{ uniqueUser.email }}</p>
-          <p>Role: {{ uniqueUser.role }}</p>
-          <button class="update-button" @click="editUser(uniqueUser)">Edit</button>
+          <div class="user-list">
+            <div class="user-list-info">
+              <div class="user-list-creds">
+                <h3>{{ uniqueUser.username }}</h3>
+                <p>Email: {{ uniqueUser.email }}</p>
+              </div>  
+              <p>Role: {{ uniqueUser.role }}</p>
+            </div>
+            <div class="user-list-buttons">
+              <button class="update-button" @click="editUser(uniqueUser)">Edit</button>
+            </div>
+          </div>
+
+          <!-- User Edit Form -->
           <div v-if="uniqueUser.id === selectedUserId" class="edit-form">
             <input v-model="updatedUser.username" type="text" placeholder="Username" />
             <textarea v-model="updatedUser.email" placeholder="Email"></textarea>
@@ -336,7 +357,7 @@ export default {
               <button class="cancel-button" @click="cancelUpdate()">Cancel</button>
             </div>
           </div>
-          <hr>
+          <!-- <hr> -->
         </li>
       </ul>
     </div>
@@ -348,13 +369,21 @@ export default {
       <div v-show="showAllLoans">
       <ul>
         <li v-for="loan in loanedItems" v-bind:key="loan">
-          <h3>Loan: {{ loan.id }}</h3>
-          <p> Item: {{loan.item.name }}</p>
-          <p> Loaned to: {{ loan.user.username }} with ID: {{ loan.user.id }}</p>
-          <p v-if="loan.returned">Item has been returned</p>
-          <p v-else>Item is Still Loaned Out</p>
-          <button class="return-button" @click="returnItem(loan.id)">Item Returned</button>
-          <hr>
+          <div class="loan-list">
+            <div class="loan-list-info">
+              <h3>Loan: {{ loan.id }} <span class="loan-ended" v-if="loan.returned === true">ENDED</span><span class="loan-active" v-else>ACTIVE</span></h3>
+              <p> Item: {{loan.item.name }}</p>
+              <p> User: {{ loan.user.username }} with ID: {{ loan.user.id }}</p>
+              <h4> Loan Date: {{ loan.loanDate }}</h4>
+              <h4> Due Date: {{ loan.dueDate }}</h4>
+              <p class="bold-text" v-if="loan.returned">Item has been returned</p>
+              <p v-else>Item is Still Loaned Out</p>
+            </div>
+            <div class="loan-list-buttons">
+              <button class="return-button" @click="returnItem(loan.id)">Item Returned</button>
+            </div>
+          </div>
+          <!-- <hr> -->
         </li>
       </ul>
     </div>
@@ -368,6 +397,69 @@ export default {
 </template>
 
 <style>
+.bold-text{
+  font-weight: bold;
+}
+
+.loan-ended { 
+  font-weight: bold;
+  color: red;
+}
+
+.loan-active{
+  font-weight: bold;
+  color: green;
+}
+
+li {
+  /* display: flex; */
+  /* width: 100%; */
+  margin-bottom: 20px;
+  border-bottom: 1px solid black;
+}
+
+.item-list,
+.user-list,
+.loan-list{
+  display: flex;
+  width: 100%;
+  /* margin-bottom: 20px; */
+}
+
+.item-list-info,
+.user-list-info,
+.loan-list-info {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.item-list-buttons,
+.user-list-buttons,
+.loan-list-buttons {
+  margin-left: auto;
+  margin-top: auto;
+  /* width: 50%;
+  top: 50%; */
+}
+
+.user-list-buttons button,
+.item-list-buttons button,
+.loan-list-buttons button {
+  margin-bottom: 1em;
+}
+
+.user-list-creds {
+  display: flex;
+  width: 100%;
+}
+
+.user-list-creds p{
+  margin-left: 10px;
+  margin-bottom: 0;
+  top: 20%;
+}
+
 .clickable-title {
   cursor: pointer;
   transition: background-color 0.3s;
@@ -385,9 +477,6 @@ ul {
   padding: 0;
 }
 
-li {
-  margin-bottom: 20px;
-}
 
 h2 {
   font-size: 40px;
@@ -401,6 +490,10 @@ h3 {
   font-weight: bold;
 }
 
+h4 {
+  margin-bottom: 10px;
+}
+
 p {
   margin-bottom: 10px;
 }
@@ -410,11 +503,14 @@ span {
   margin-bottom: 5px;
 }
 
+.modal-cancel-button,
 .modal-button,
 .return-button,
 .delete-button,
-.update-button {
-  background-color: hsl(0, 0%, 0%);
+.update-button,
+.save-button,
+.cancel-button {
+  /* background-color: hsl(0, 0%, 0%); */
   color: rgb(255, 255, 255);
   border: none;
   padding: 8px 16px;
@@ -425,28 +521,42 @@ span {
   float: right;
 }
 
-.modal-button:hover,
+/* .modal-button:hover,
 .return-button:hover,
 .delete-button:hover,
 .update-button:hover {
   background-color: #a90000;
-}
-
-/* .update-button {
-  background-color: hsl(0, 0%, 0%);
-  color: rgb(255, 255, 255);
-  border: none;
-  padding: 8px 16px;
-  margin-left: 3px;
-  margin-bottom: -10px;
-  border-radius: 4px;
-  cursor: pointer;
-  float: right;
-}
-
-.update-button:hover {
-  background-color: #a90000;
 } */
+
+.modal-cancel-button,
+.update-button,
+.save-button,
+.return-button {
+  background-color: green;
+}
+
+.modal-cancel-button:hover,
+.update-button:hover,
+.save-button:hover,
+.return-button:hover {
+  background-color: rgba(0, 128, 0, 0.483);
+} 
+
+.modal-button,
+.delete-button,
+.cancel-button {
+  background-color: red;
+}
+
+.modal-button:hover,
+.delete-button:hover,
+.cancel-button:hover {
+  background-color: rgba(255, 0, 0, 0.483);
+} 
+
+.form-buttons {
+  margin-bottom: 20px;
+}
 
 .loan {
   background-color: #f8f8f8;
@@ -480,7 +590,7 @@ span {
   margin-left: 10px;
 }
 
-.save-button {
+/* .save-button {
   background-color: #4caf50;
   color: white;
   border: none;
@@ -496,7 +606,7 @@ span {
   padding: 8px 12px;
   border-radius: 3px;
   cursor: pointer;
-}
+} */
 
 .unavailable {
   color: red;
