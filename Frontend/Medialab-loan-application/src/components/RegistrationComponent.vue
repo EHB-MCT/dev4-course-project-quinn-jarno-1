a5b6818ea97b65f6ae68cfb62dadf411aabb290a
QuinnJan <quinn.janssens@student.ehb.be>
2023-05-17 14:12:38 +0100

    Styling Registration form

--- Frontend/Medialab-loan-application/src/components/RegistrationComponent.vue
+++ Frontend/Medialab-loan-application/src/components/RegistrationComponent.vue
@@ -50,27 +50,30 @@
           role: this.role
         };
         console.log(userData);
+      },
+      back(){
+        this.$router.push('/');
       }
     }
   };
   </script>
   <template>
-    <div>
-      <h2>Registration Form</h2>
-      <form @submit.prevent="registerUser">
-        <div>
+    <div class="registration-container">
+      <form class="registration-form" @submit.prevent="registerUser">
+        <h2>Registration Form</h2>
+        <div class="form-group">
           <label for="username">Username:</label>
-          <input type="text" id="username" v-model="username" required>
+          <input type="text" id="username" placeholder="Username" v-model="username" required>
         </div>
-        <div>
+        <div class="form-group">
           <label for="password">Password:</label>
-          <input type="password" id="password" v-model="password" required>
+          <input type="password" id="password" placeholder="password" v-model="password" required>
         </div>
-        <div>
+        <div class="form-group">
           <label for="email">Email:</label>
-          <input type="email" id="email" v-model="email" required>
+          <input type="email" id="email" placeholder="email" v-model="email" required>
         </div>
-        <div>
+        <div class="form-group">
           <label for="role">Role:</label>
           <select id="role" v-model="role" required>
             <option value="">Select Role</option>
@@ -78,7 +81,86 @@
             <option value="user">User</option>
           </select>
         </div>
-        <button type="submit">Register</button>
+        <div class="form-actions">
+          <button class="back-button" @click="back">Back</button>
+          <button type="submit" class="register-button">Register</button>
+        </div>
       </form>
     </div>
   </template>
+  
+  <style>
+  .registration-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  
+  .registration-form {
+    background-color: #f4f4f4;
+    padding: 20px;
+    border-radius: 4px;
+    width: 300px;
+  }
+  
+  h2 {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  
+  input[type="text"],
+  input[type="password"],
+  input[type="email"],
+  select {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin: 5px;
+  }
+  
+  .form-actions {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+  }
+  
+  .register-button {
+    background-color: #ce0000;
+    color: #fff;
+    border: none;
+    padding: 8px 60px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin: 10px;
+  }
+
+  .back-button {
+    background-color: #ce0000;
+    color: #fff;
+    border: none;
+    padding: 8px 12px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin: 10px;
+    margin-right: 12px;
+  }
+  
+  .register-button:hover,
+  .back-button:hover {
+    background-color: #000000;
+  }
+  </style>
+  
