df83a6d9f29eddc45ffaf6e173950b1e7a61dfcf
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-05-17 14:29:27 +0200

    Fixed Create Item, added Styling

--- Frontend/Medialab-loan-application/src/components/ItemsComponent.vue
+++ Frontend/Medialab-loan-application/src/components/ItemsComponent.vue
@@ -63,30 +63,10 @@ mounted() {
     this.filterItems(this.searchQuery);
     },
 
-    createItem(createdItem) {
-            fetch('http://localhost:9000/items', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(createdItem),
-            })
-                .then(response => {
-                    if (response.ok) {
-                      this.items.push(createdItem)
-                        console.log("Item creation OK")
-                    } else {
-                        throw new Error('Could not create item.');
-                    }
-                })
-                .catch(error => {
-                    console.error(error);
-           });
-        },
-
-  // Get Logged In User
-  getUser() {
-    fetch(`http://localhost:9000/users/token/${localStorage.getItem('authToken')}`)
+    
+    // Get Logged In User
+    getUser() {
+      fetch(`http://localhost:9000/users/token/${localStorage.getItem('authToken')}`)
       .then(response => response.json())
       .then(data => {
         this.user = data;
@@ -96,7 +76,7 @@ mounted() {
         console.error(error);
       });
   },
-
+  
   // Lend out an item
   loanItem(userId, itemId){
     const token = localStorage.getItem('authToken');
@@ -119,29 +99,51 @@ mounted() {
       .catch(error => {
         console.error(error);
       });
+    },
+    
+  createItem() {
+    fetch('http://localhost:9000/items', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.createdItem),
+    })
+        .then(response => {
+            if (response.ok) {
+              this.items.push(this.createdItem)
+              console.log(`Item With Name ${this.createdItem.name} Created`)
+            } else {
+                throw new Error(`Could not create item. ${this.createdItem} `);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+    });
   },
 
   // Delete Item
-  deleteItem(itemId) {
-    fetch(`http://localhost:9000/items/${itemId}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => {
-        if (response.ok) {
-        const index = this.items.findIndex(item => item.id === itemId);
-        if (index !== -1) {
-          this.items.splice(index, 1);
-        }
-      } else {
-        console.error('Item deletion failed: Item may be Loaned Out');
-      }
-    }).catch(error => {
-        console.error(error);
-      });
-  },
+  // deleteItem(itemId) {
+  //   fetch(`http://localhost:9000/items/${itemId}`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then(response => {
+  //       console.log(`Item With ID ${itemId} Deleted`)
+  //       if (response.ok) {
+  //       const index = this.items.findIndex(item => item.id === itemId);
+  //       if (index !== -1) {
+  //         this.items.splice(index, 1);
+  //       }
+  //     } else {
+  //       console.error('Item deletion failed: Item may be Loaned Out');
+  //     }
+  //   }).catch(error => {
+  //       console.error(error);
+  //     });
+  // },
 
   // Edit Item
   editItem(item) {
@@ -150,30 +152,31 @@ mounted() {
       console.log("response OK")
     },
 
-  updateItem() {
-    fetch(`http://localhost:9000/items/update/${this.selectedItemId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(this.updatedItem)
-    })
-      .then(response => {
-        if (response.ok) {
-            console.log("response OK")
-            const index = this.items.findIndex(item => item.id === this.updatedItem.id);
-            if (index !== -1) {
-              this.items.splice(index, 1, this.updatedItem);
-            }
-            this.cancelEdit();
-          } else {
-            console.error('Item update failed.');
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
-  },
+  // updateItem() {
+  //   fetch(`http://localhost:9000/items/update/${this.selectedItemId}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(this.updatedItem)
+  //   })
+  //     .then(response => {
+  //       if (response.ok) {
+  //           console.log("response OK")
+  //           const index = this.items.findIndex(item => item.id === this.updatedItem.id);
+  //           if (index !== -1) {
+  //             this.items.splice(index, 1, this.updatedItem);
+  //           }
+  //           this.cancelEdit();
+  //         } else {
+  //           console.error('Item update failed.');
+  //         }
+  //       })
+  //       .catch(error => {
+  //         console.error(error);
+  //       });
+  // },
+  
   cancelEdit() {
       this.selectedItemId = null;
       this.updatedItem = {
@@ -184,15 +187,11 @@ mounted() {
       };
 
  },
- // this doesnt get updated through the updateItem method but through the create loan method
- setLoanStatus(isLoanedOut) {
-      this.updatedItem.isLoanedOut = isLoanedOut;
-    }
 },
 
  updated() {
   
- console.log(this.items);
+//  console.log(this.items);
 }
  }
 
@@ -209,7 +208,8 @@ mounted() {
   <div>
         <input type="text" v-model="createdItem.name" placeholder="Name" />
         <input type="text" v-model="createdItem.description" placeholder="Description" />
-        <button @click="createItem">Create item</button>
+        <input type="hidden" v-model="createdItem.isLoanedOut" />
+        <button @click="createItem()">Create item</button>
       </div>
   <div class="search-container">
     <input class="search-input" type="text" v-model="searchQuery" placeholder="Search item" />
@@ -218,48 +218,34 @@ mounted() {
           <ul>
             <div v-if="searchQuery">
                 <li v-for="item in filteredItems" :key="item">
-                <h3>{{ item.name }}</h3>
-                <p>{{ item.description }}</p>
-                <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
-                <span class="available" v-else>Available</span>
-                <div v-if="user && user.role === 'admin'">
-                <button class="update-button" @click="editItem(item)">Update</button>
-                <div v-if="item.id === selectedItemId" class="edit-form">
-                  <input v-model="updatedItem.name" type="text" placeholder="Name" />
-                  <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
-        
-                  <div class="form-buttons">
-                  <button class="save-button" @click="updateItem">Save</button>
-                  <button class="cancel-button" @click="cancelEdit">Cancel</button>
+                  <div class="item-list">
+                    <div class="item-list-info">
+                      <h3>{{ item.name }}</h3>
+                      <p>{{ item.description }}</p>
+                      <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+                      <span class="available" v-else>Available</span>
+                    </div>
+                    <div class="item-list-buttons">
+                      <button class="loan-button" @click="loanItem(user.id, item.id)">Lend Out</button>
+                    </div>
                   </div>
-                  </div>
-                <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
-              </div>
-                <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button>
-                <hr>
+                  <!-- <hr> -->
               </li>
             </div>
             <div v-else>
               <li v-for="item in items" :key="item">
-                <h3>{{ item.name }}</h3>
-                <p>{{ item.description }}</p>
-                <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
-                <span class="available" v-else>Available</span>
-                <div v-if="user && user.role === 'admin'">
-                <button class="update-button" @click="editItem(item)">Update</button>
-                <div v-if="item.id === selectedItemId" class="edit-form">
-                  <input v-model="updatedItem.name" type="text" placeholder="Name" />
-                  <textarea v-model="updatedItem.description" placeholder="Description"></textarea>
-          
-                  <div class="form-buttons">
-                  <button class="save-button" @click="updateItem">Save</button>
-                  <button class="cancel-button" @click="cancelEdit">Cancel</button>
+                <div class="item-list">
+                  <div class="item-list-info">
+                    <h3>{{ item.name }}</h3>
+                    <p>{{ item.description }}</p>
+                    <span class="unavailable" v-if="item.isLoanedOut">Unavailable</span>
+                    <span class="available" v-else>Available</span>
                   </div>
+                  <div class="item-list-buttons">
+                    <button class="loan-button" @click="loanItem(user.id, item.id)">Lend Out</button>
                   </div>
-                <button class="delete-button" @click="deleteItem(item.id)">Delete</button>
-              </div>
-                <button class="delete-button" @click="loanItem(user.id, item.id)">Lend Out</button>
-                <hr>
+                </div>
+                  <!-- <hr> -->
               </li>
             </div>
 
@@ -381,13 +367,18 @@ mounted() {
   cursor: pointer;
 }
 
-.cancel-button {
-  background-color: #f44336;
-  color: white;
-  border: none;
+.loan-button {
+  background-color: white;
+  color: black;
+  border: black solid 1px;
   padding: 8px 12px;
-  border-radius: 3px;
+  border-radius: 30px;
   cursor: pointer;
+}
+
+.loan-button:hover {
+  background-color: rgba(0, 128, 0, 0.531);
+  transform: scale(1.1);
 }
 
 .unavailable {
@@ -398,5 +389,43 @@ mounted() {
 .available {
   color: green;
   font-weight: bold;
+}
+
+li {
+  /* display: flex; */
+  /* width: 100%; */
+  margin-bottom: 20px;
+  border-bottom: 1px solid black;
+}
+
+.item-list,
+.user-list,
+.loan-list{
+  display: flex;
+  width: 100%;
+  /* margin-bottom: 20px; */
+}
+
+.item-list-info,
+.user-list-info,
+.loan-list-info {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.item-list-buttons,
+.user-list-buttons,
+.loan-list-buttons {
+  margin-left: auto;
+  margin-top: auto;
+  /* width: 50%;
+  top: 50%; */
+}
+
+.user-list-buttons button,
+.item-list-buttons button,
+.loan-list-buttons button {
+  margin-bottom: 1em;
 }
   </style>
